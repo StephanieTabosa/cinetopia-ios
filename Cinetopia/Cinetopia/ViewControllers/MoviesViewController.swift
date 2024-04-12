8396f04ade23c1d764d15d1acad3e4ae2d8664f7
Stèphanie Tabosa <stephanie.barreto@hotmail.com>
2024-04-11 22:48:12 -0300

    add images and create tableView

---
+++ Cinetopia/Cinetopia/ViewControllers/MoviesViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MoviesViewController.swift
+//  Cinetopia
+//
+//  Created by Stèphanie Tabosa on 09/04/24.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "movieCell")
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
+        addSubviews()
+        setupConstraints()
+        setupNavigationBar()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    
+    private func setupNavigationBar() {
+        title = "Filmes populares"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.white
+        ]
+        navigationItem.setHidesBackButton(true, animated: true)
+    }
+}
+
+extension MoviesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = movies[indexPath.row].title
+        configuration.textProperties.color = .accent
+        cell.contentConfiguration = configuration
+        cell.backgroundColor = .clear
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
